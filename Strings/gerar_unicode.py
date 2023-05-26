1c22fa5b9a5d76b37794c1b6367a88063c9735cf
giordanidev <giordanidev@gmail.com>
2023-05-26 17:04:21 -0300

    gerador de unicode iniciado

---
+++ Strings/gerar_unicode.py
@@ -0,0 +1,16 @@
+variaveis_unicode = [
+    ["°", "&deg;"],
+    ["ª", "&ordf;"],
+    ["à", "&agrave;"],
+    ["á", "&aacute;"],
+    ["â", "&acirc;"],
+    ["ã", "&atilde"],
+    ["ç", "&ccedil;"],
+    ["é", "&eacute;"],
+    ["ê", "&ecirc;"],
+    ["í", "&iacute;"],
+    ["ó", "&oacute;"],
+    ["ô", "&ocirc;"],
+    ["õ", "&otilde;"],
+    ["ú", "&uacute;"]
+]
