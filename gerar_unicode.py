5055f5d4e2a378fb810dc5a56cd925ee5d4ca342
Giordani.dev <giordanisarturi@gmail.com>
2023-05-30 04:02:20 -0300

    removendo arquivos não necessários novas traduções novo teste

--- gerar_unicode.py
+++ gerar_unicode.py
@@ -2,34 +2,44 @@ import sys
 import codecs
 
 def encode():
-    path = ".\\strings"
-    basename = "client_strings_ui_test.xml"
-    filename = path + "\\" + basename
+    files = [
+        ".\\PAK\\data\\strings\\client_strings_ui.xml",
+        ".\\PAK\\data\\strings\\stringtable_dialog.xml",
+        ".\\PAK\\data\\cutscene\\cs_ab1_001.xml"
+    ]
+    #path = ".\\strings"
+    #basename = "client_strings_ui_test.xml"
+    #filename = path + "\\" + basename
     #file = open(filename, "rt")
-    f = codecs.open(filename, encoding='utf-8')
-    contents = f.read()
-    f.close
+    for filename in files:
+        print(filename)
+        f = codecs.open(filename, encoding='utf-8')
+        contents = f.read()
+        f.close
 
-    char_unicode = {
-        "º": "&ordm;",
-        "ª": "&ordf;",
-        "à": "&agrave;",
-        "á": "&aacute;",
-        "â": "&acirc;",
-        "ã": "&atilde;",
-        "ç": "&ccedil;",
-        "é": "&eacute;",
-        "ê": "&ecirc;",
-        "í": "&iacute;",
-        "ó": "&oacute;",
-        "ô": "&ocirc;",
-        "õ": "&otilde;",
-        "ú": "&uacute;"
-    }
+        char_unicode = {
+            "º": "&ordm;",
+            "ª": "&ordf;",
+            "à": "&agrave;",
+            "á": "&aacute;",
+            "â": "&acirc;",
+            "ã": "&atilde;",
+            "ç": "&ccedil;",
+            "é": "&eacute;",
+            "ê": "&ecirc;",
+            "í": "&iacute;",
+            "ó": "&oacute;",
+            "ô": "&ocirc;",
+            "õ": "&otilde;",
+            "ú": "&uacute;",
+            "&apos;": "'",
+            "´": "'",
+            "`": "'"
+        }
 
-    contents="".join((char_unicode.get(x, x) for x in contents))
+        contents="".join((char_unicode.get(x, x) for x in contents))
 
-    f = open(filename, 'w', encoding='utf-8')
-    f.write(contents)
+        f = open(filename, 'w', encoding='utf-8')
+        f.write(contents)
 
 encode()
