d073e5921df4e2587e689e2660c8ce9840d8c6c5
Giordani.dev <giordanisarturi@gmail.com>
2023-10-24 15:14:26 -0300

    atualizado

--- instalar_pak.py
+++ instalar_pak.py
@@ -1,4 +1,4 @@
-import os, shutil, subprocess
+import os, shutil, subprocess, zipfile
 
 #print("Gerando arquivo .PAK!")
 #Não está funcionando para o arquivo \strings\client_strings_item2.xml
@@ -7,7 +7,8 @@ import os, shutil, subprocess
 
 orig_repack = ".\\REPACK\\data_ptBR.pak"
 dest_repack = "E:\\JOGOS\\aionclassic\\l10n\\ENG\\data\\data_ptBR.pak"
-dest_test = ".\\teste\\data_ptBR.pak"
+dest_test = ".\\arquivo\\data_ptBR.pak"
+zip_test = ".\\arquivo\\data_ptBR.zip"
 
 print(f"Instalando orig_repack: {orig_repack} -> dest_repack: '{dest_repack}'")
 print(f"Verificando se arquivo existe: {dest_repack}")
@@ -42,6 +43,15 @@ else:
         print("Diretório dest_test criado.")
 try:
     shutil.copy2(orig_repack, dest_test)
+    shutil.copy2(orig_repack, ".\\data_ptBR.pak")
     print("Arquivo de teste instalado com sucesso!")
 except:
          print("ERRO! Não foi possível instalar.")
+try:
+    with zipfile.ZipFile(zip_test, 'w') as file:
+        file.write(".\\data_ptBR.pak")
+    with zipfile.ZipFile(zip_test, 'r') as file:
+        print(file.namelist())
+    os.remove(".\\data_ptBR.pak")
+except:
+         print("Não foi possível criar o arquivo zip.")
