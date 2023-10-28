9405f64f1f3c05c56305c97cd5cb4354ac2f8b4d
Giordani.dev <giordanisarturi@gmail.com>
2023-10-28 19:08:39 -0300

    teste

--- 0_parser.py
+++ 0_parser.py
@@ -1,37 +1,37 @@
 import xml.etree.ElementTree as parser
+import shutil, os
 
-arquivos = ["strings\\client_strings_ui.xml"
+arquivos = [
+    #"strings\\client_strings_ui.xml",
+    #"strings\\client_strings_msg.xml",
+    "strings\\stringtable_dialog.xml",
+    "strings\\client_strings_item.xml",
+    "strings\\client_strings_item2.xml",
+    "strings\\client_strings_skill.xml",
+    "strings\\client_strings_level.xml"
 ]
 
-"""
-arquivos = ["strings\\client_strings_ui.xml",
-        "strings\\client_strings_msg.xml",
-        "strings\\stringtable_dialog.xml",
-        "strings\\client_strings_item.xml",
-        "strings\\client_strings_item2.xml",
-        "strings\\client_strings_skill.xml",
-        "strings\\client_strings_level.xml"
-]
-"""
-
-originais = ["_data_eu\\", "_Data_na\\"]
-contagem = 0
+#originais = ["_data_eu\\", "_Data_na\\"]
+originais = ["_Data_na\\"]
+parsed = "_parsed\\"
 
 for arquivo in arquivos:
-
+    print(f"Arquivo: {arquivo}")
     arquivo_tree = parser.parse(arquivo)
     arquivo_root = arquivo_tree.getroot()
 
     for original in originais:
-        original = originais+arquivo
+        contagem = 0
+        original = original+arquivo
+        dest = parsed+original
+        print(f"Original: {original} \nFinal: {dest}")
         original_tree = parser.parse(original)
         original_root = original_tree.getroot()
-    
-        print("Arquivo: "+arquivo)
+
         for linha in arquivo_root:
             contagem += 1
             linhas_total = len(arquivo_root)
-            print("Strings: "+str(contagem)+"/"+str(linhas_total), end="\r")
+            print(f"Strings: {str(contagem)}/{str(linhas_total)}", end="\r")
 
             linha_id = linha.find('id').text
             linha_nome = linha.find('name').text
@@ -41,8 +41,26 @@ for arquivo in arquivos:
             try:
                 linha_body = linha.find('body').text
                 #print(linha_body)
-                linha_original = original_root.find(".//*[name='"+linha_nome+"']")
+                linha_original = original_root.find(f".//*[name='{linha_nome}']")
                 linha_original.find('body').text = linha_body
             except: "" #print("ERRO! "+linha_id)
+        print(f"Strings: {str(contagem)}/{str(linhas_total)} - COMPLETADO!", end="\r")
         print("")
-        original_tree.write(original)
+
+        if os.path.isfile(dest):
+            print("- Arquivo existe, removendo.")
+            os.remove(dest)
+            print("- Arquivo removido.")
+        else:
+            print("- Arquivo não existe.")
+            if not os.path.isdir(os.path.dirname(dest)):
+                    print("- Diretório não existe. Criando diretório.")
+                    os.makedirs(os.path.dirname(dest))
+                    print("- Diretório criado.")
+
+        print(f"- Copiando original: {original} -> dest: {dest}")
+        shutil.copy2(original, dest)
+        print("- Copiado com sucesso!")
+
+        original_tree.write(dest)
+        print("============== Arquivo finalizado! ==============")
