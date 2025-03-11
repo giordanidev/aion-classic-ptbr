8a225b9ead54d804aab44ddce69f1deb85c677fc
Giordani <giordanisarturi@gmail.com>
2025-03-11 17:26:55 -0300

    arquivos 1.9

---
+++ main.py
@@ -0,0 +1,241 @@
+#sys.exit()
+import lxml.etree as parser
+import os, time, codecs, shutil, zipfile, subprocess, sys, re
+from pathlib import Path
+
+print(f"+|+|+ SCRIPT INICIADO")
+start_time_python = time.monotonic()
+caminho_originais = ".\\_originais"
+caminho_downloads = ".\\_download"
+caminho_traduzidos = ".\\_traduzidos"
+caminho_parsed = ".\\_parsed"
+
+arquivos_originais = [name for name in os.listdir(caminho_originais) if os.path.isdir(os.path.join(caminho_originais, name))] #puxa os diretorios na pasta _originais
+arquivos = []
+for caminho, _, traduzir in os.walk(caminho_traduzidos):
+    for arquivo in traduzir:
+        arquivos.append(re.sub(caminho_traduzidos, '', os.path.join(caminho, arquivo))) #puxa o caminho completo de todos os arquivos já traduzidos
+#quando "ignorar" for "Verdadeiro", o arquivo será traduzido por completo
+#usado em arquivos que a descrição é o nome do item
+ignorar_parse = [
+                "ui.xml",
+                "msg.xml",
+                "dialog.xml",
+                "level.xml"
+                ]
+# Esses arquivos não devem passar pelo parse por serem HTML.
+# Eles vão direto para a transcrição unicode e o repack.
+nao_parse = [
+            
+            ]
+def verificar_arquivos(contagem_arquivo, iteracoes, dest):
+    print(f"[{contagem_arquivo}/{iteracoes}] Verificando arquivo: {dest}", end="\r")
+    if os.path.isfile(dest):
+        print(f"[{contagem_arquivo}/{iteracoes}] Arquivo existe: '{dest}' :: Removendo.", end="\r")
+        os.remove(dest)
+        print(f"[{contagem_arquivo}/{iteracoes}] Arquivo existe: '{dest}' :: Removido.")
+    else:
+        print(f"[{contagem_arquivo}/{iteracoes}] Arquivo não existe: '{dest}'.")
+        if not os.path.isdir(os.path.dirname(dest)):
+            print(f"[{contagem_arquivo}/{iteracoes}] Diretório não existe :: Criando diretório: '{os.path.dirname(dest)}'.", end="\r")
+            os.makedirs(os.path.dirname(dest))
+            print(f"[{contagem_arquivo}/{iteracoes}] Diretório não existe :: Diretório criado: '{os.path.dirname(dest)}'.")
+
+def parsing():
+    print(f">>>>> INICIANDO PARSE", end="\r")
+    start_time_total = time.monotonic()
+    contagem_arquivo = 1
+    arquivos_len = len(arquivos)
+    iteracoes = arquivos_len*len(arquivos_originais)
+
+    print(f">>>>> INICIANDO PARSE :: {iteracoes} arquivos encontrados :: {len(caminho_originais)} iteração(ões) :: Total de {iteracoes} parses")
+
+    for original in arquivos_originais:
+
+        print(f"##### ITERAÇÃO: {original}")
+        start_time_original = time.monotonic()
+        ignorar_parse_desc = False
+        for traducao in arquivos:
+            for texto in traducao.split("_"):
+                if texto in ignorar_parse:
+                    ignorar_parse_desc = True
+
+            start_time_traducao = time.monotonic()
+            contagem = 0
+            arquivo_original = f"{caminho_originais}\\{traducao}"
+            arquivo_traduzido = f"{caminho_traduzidos}\\{traducao}"
+            arquivo_parsed = f"{caminho_parsed}\\{traducao}"
+            print(f"[{contagem_arquivo}/{iteracoes}] Arquivo: {traducao}")
+            print(caminho_originais)
+            print(original)
+            print(traducao)
+            print(arquivo_original)
+
+            original_tree = parser.parse(arquivo_original)
+            original_root = original_tree.getroot()
+            original_len = len(original_root)
+
+            traducao_tree = Path(arquivo_traduzido)
+            traducao_root = parser.iterparse(traducao_tree, events=('start', 'end'))
+
+            novo_parse = parser.Element('strings')
+
+            start_time_line = time.monotonic()
+            for original_string in original_root:
+                texto_traducao = None
+                if original_string.find('name') is not None:
+                    if original_string.find('body') is not None:
+                        for event, traducao_string in traducao_root:
+                            if event == 'end':
+                                if (traducao_string.tag == 'string'):
+                                    if traducao_string.find('name') is not None:
+                                        if traducao_string.find('body') is not None:
+                                            if traducao_string.find('name').text == original_string.find('name').text:
+                                                texto_traducao = traducao_string
+                                                break
+
+                if texto_traducao == None:
+                    texto_traducao = original_string
+                novo_string = parser.SubElement(novo_parse, texto_traducao.tag)
+                
+                for elemento in texto_traducao:
+                    # Quando "ignorar" for "Verdadeiro", o arquivo será traduzido por completo
+                    # 
+                    if ignorar_parse_desc == False:
+                        if "DESC" in elemento.text.split("_"):
+                            parser.SubElement(novo_string, elemento.tag).text = texto_traducao.find(elemento.tag).text
+                        else:
+                            parser.SubElement(novo_string, elemento.tag).text = original_string.find(elemento.tag).text
+                    else:
+                        parser.SubElement(novo_string, elemento.tag).text = texto_traducao.find(elemento.tag).text
+            """
+                execution_time = "%.2f" % (time.monotonic() - start_time_line)
+                sys.stdout.write(f"[{contagem_arquivo}/{iteracoes}] Parsing: {str(contagem)}/{original_len} ({execution_time}s)"+"\r")
+                contagem += 1
+            print("")
+            """
+            
+            verificar_arquivos(contagem_arquivo, iteracoes, arquivo_parsed)
+            
+            parser.ElementTree(novo_parse).write(arquivo_parsed, encoding='utf-8', xml_declaration=True, pretty_print=True)
+            execution_time = "%.2f" % (time.monotonic() - start_time_traducao)
+            print(f"[{contagem_arquivo}/{iteracoes}] Arquivo salvo em '{arquivo_parsed}' ({execution_time}s)")
+            contagem_arquivo += 1
+
+        execution_time = "%.2f" % (time.monotonic() - start_time_original)
+        print(f"##### ITERAÇÃO FINALIZADA: {original} ({execution_time}s)")
+        
+    execution_time = "%.2f" % (time.monotonic() - start_time_total)
+    print(f"!!!!! PARSE FINALIZADO ({execution_time}s)")
+
+def unicode():
+    print(f">>>> INICIANDO TRANSCRIÇÃO", end="\r")
+    start_time_total = time.monotonic()
+    arquivos.extend(nao_parse)
+
+    contagem_arquivo = 1
+    arquivos_len = len(arquivos)
+    iteracoes = arquivos_len*len(caminho_originais)
+
+    print(f">>>>> INICIANDO TRANSCRIÇÃO :: {iteracoes} arquivos encontrados :: {len(caminho_originais)} iteração(ões) :: Total de {iteracoes} transcrições")
+
+    for original in caminho_originais:
+        
+        start_time_original = time.monotonic()
+        print(f"##### ITERAÇÃO: {original}")
+
+        for arquivo in arquivos:
+            start_time_arquivo = time.monotonic()
+            orig = f"{caminho_traduzidos}\\{original}\\{arquivo}"
+            dest = f"{caminho_downloads}\\z_{original}_ptBR\\{arquivo}"
+
+            verificar_arquivos(contagem_arquivo, iteracoes, dest)
+
+            print(f"[{contagem_arquivo}/{iteracoes}] Copiando arquivo: '{orig}' -> '{dest}'", end="\r")
+            if os.path.isfile(orig):
+                shutil.copy2(orig, dest)
+            else:
+                shutil.copy2(f".\\{arquivo}", dest)
+            print(f"[{contagem_arquivo}/{iteracoes}] Copiando arquivo: '{orig}' -> '{dest}' :: Arquivo copiado")
+
+            f = codecs.open(dest, encoding='utf-8')
+            contents = f.read()
+            f.close
+
+            char_unicode = {
+                "º": "&ordm;", "ª": "&ordf;",
+                "À": "&Agrave;", "Á": "&Aacute;", "Â": "&Acirc;", "Ã": "&Atilde;",
+                "à": "&agrave;", "á": "&aacute;", "â": "&acirc;", "ã": "&atilde;",
+                "Ç": "&Ccedil;",
+                "ç": "&ccedil;",
+                "É": "&Eacute;", "Ê": "&Ecirc;",
+                "é": "&eacute;", "ê": "&ecirc;",
+                "Í": "&Iacute;",
+                "í": "&iacute;",
+                "Ó": "&Oacute;", "Ô": "&Ocirc;", "Õ": "&Otilde;",
+                "ó": "&oacute;", "ô": "&ocirc;", "õ": "&otilde;",
+                "Ú": "&Uacute;",
+                "ú": "&uacute;",
+                "&apos;": "'", "´": "'", "`": "'", "’": "'", "‘": "'", " ": " ", "–": "-"
+            }
+
+            contents="".join((char_unicode.get(x, x) for x in contents))
+            
+            print(f"[{contagem_arquivo}/{iteracoes}] Transcrição finalizada", end="\r")
+
+            f = open(dest, 'w', encoding='utf-8', newline='')
+            f.write(contents)
+            execution_time = "%.2f" % (time.monotonic() - start_time_arquivo)
+
+            print(f"[{contagem_arquivo}/{iteracoes}] Transcrição finalizada :: Arquivo salvo ({execution_time}s)")
+            contagem_arquivo += 1
+
+        execution_time = "%.2f" % (time.monotonic() - start_time_original)
+        print(f"##### ITERAÇÃO FINALIZADA: {original} ({execution_time}s)")
+
+    execution_time = "%.2f" % (time.monotonic() - start_time_total)
+    print(f"!!!!! TRANSCRIÇÃO FINALIZADA ({execution_time}s)")
+
+def repack():
+    start_time_total = time.monotonic()
+    print(f">>>> INICIANDO REPACK")
+    iteracoes = len(caminho_originais)
+    contagem_arquivo = 1
+    print(f"[{contagem_arquivo}/{iteracoes}] Gerando arquivo .PAK!", end="\r")
+    subprocess.run([".\\Aion_Encdec.exe", "-r"])
+    print(f"[{contagem_arquivo}/{iteracoes}] Gerando arquivo .PAK! :: Arquivo gerado")
+    for original in caminho_originais:
+        start_time_original = time.monotonic()
+        print(f"##### ITERAÇÃO: {original}")
+
+        orig = f"REPACK\\z_{original}_ptBR.pak"
+        dest_temp = f"z_{original}_ptBR.pak"
+        dest = f"{caminho_downloads}\\z_{original}_ptBR.zip"
+
+        verificar_arquivos(contagem_arquivo, iteracoes, dest)
+
+        #COPIAR PAK PARA A PASTA TEMPORÁRIA PRINCIPAL PARA CRIAR O ZIP
+        shutil.copy2(orig, dest_temp)
+        print(f"[{contagem_arquivo}/{iteracoes}] Arquivo copiado: {dest_temp}")
+
+        with zipfile.ZipFile(dest, 'w') as file:
+            file.write(dest_temp)
+        #with zipfile.ZipFile(zip_dest, 'r') as file:
+        #    print(file.namelist())
+
+        os.remove(dest_temp)
+        print(f"[{contagem_arquivo}/{iteracoes}] Arquivo zip criado com sucesso: '{dest}'.")
+        contagem_arquivo += 1
+
+        execution_time = "%.2f" % (time.monotonic() - start_time_original)
+        print(f"##### ITERAÇÃO FINALIZADA: {original} ({execution_time}s)")
+
+    execution_time = "%.2f" % (time.monotonic() - start_time_total)
+    print(f"!!!!! REPACK FINALIZADO ({execution_time}s)")
+
+parsing()
+#unicode()
+#repack()
+
+execution_time = "%.2f" % (time.monotonic() - start_time_python)
+print(f"+|+|+ SCRIPT CONCLUÍDO ({execution_time}s)")
